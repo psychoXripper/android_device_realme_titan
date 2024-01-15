bc3199f0dcaac554c7fdcc8e40af4ae99def56f1
francium-96 <johntherippper96@gmail.com>
2024-01-15 21:15:57 +0600

    titan: Build default health HIDL

--- device.mk
+++ device.mk
@@ -48,6 +48,14 @@ PRODUCT_PACKAGES += \
     checkpoint_gc \
     otapreopt_script
 
+
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
