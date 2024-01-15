345e9f4362b060ecd31cce203b8d7f3106e2e244
francium-96 <johntherippper96@gmail.com>
2024-01-15 21:06:28 +0600

    titan: Build MediaTek boot control HAL

--- device.mk
+++ device.mk
@@ -14,7 +14,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression_retrofit.mk)
 
+
 # A/B
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-mtkimpl \
+    android.hardware.boot@1.2-mtkimpl.recovery \
+    android.hardware.boot@1.2-service
+
 PRODUCT_PACKAGES += \
     update_engine \
     update_engine_sideload \
