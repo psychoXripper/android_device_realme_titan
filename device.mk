62447ae9acdc7e91d04ccab95ea10f348593f152
francium-96 <johntherippper96@gmail.com>
2024-01-16 20:05:05 +0600

    titan: Configure Vibrator stack

--- device.mk
+++ device.mk
@@ -160,5 +160,14 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.3.vendor \
     android.hardware.usb.gadget@1.1.vendor
 
+
+# Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service.titan
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/realme/titan/titan-vendor.mk)
