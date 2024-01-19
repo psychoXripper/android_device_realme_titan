5eba383afb30782c723c3159f290d0ea7d3ce33b
francium-96 <johntherippper96@gmail.com>
2024-01-19 17:43:33 +0600

    titan: Add a shim to link sensor blobs against `android.hardware.sensors@1.0-convert`

--- extract-files.sh
+++ extract-files.sh
@@ -68,8 +68,14 @@ function blob_fixup {
         vendor/etc/init/android.hardware.neuralnetworks@1.3-service-mtk-neuron.rc)
             sed -i 's/start/enable/' "$2"
             ;;
+        vendor/bin/mnld|\
+        vendor/lib*/hw/android.hardware.sensors@2.X-subhal-mediatek.so|\
+        vendor/lib*/libaalservice.so)
+            "$PATCHELF" --add-needed "libshim_sensors.so" "$2"
+            ;;
         vendor/lib*/hw/vendor.mediatek.hardware.pq@2.14-impl.so)
             "$PATCHELF" --replace-needed "libutils.so" "libutils-v32.so" "$2"
+            "$PATCHELF" --add-needed "libshim_sensors.so" "$2"
             ;;
         vendor/lib*/libmtkcam_stdutils.so)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "$2"
